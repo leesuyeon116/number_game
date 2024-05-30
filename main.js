7b6db9674f5bce6dc35de5890b419ec6e67f1ec0
leesuyeon116 <suyeon873088@naver.com>
2024-05-30 22:54:05 +0900

    reset 버튼 위치 수정해야 함

--- main.js
+++ main.js
@@ -1,55 +1,58 @@
-let computerNum = 0;
+let computerNumber = 0;
 let playButton = document.getElementById("play-button");
-let userInput = document.getElementById("user-input");
-let resultArea = document.getElementById("result-area");
-let resetButton = document.getElementById("reset-button");
-let chances = 5;
-let gameOver = false;
+let resetButton = document.getElementById("button-reset");
+let userInput = document.querySelector("#user-input");
+let resultAreaImg = document.querySelector(".main-img");
+let resultText = document.querySelector(".result-text");
 let chanceArea = document.getElementById("chance-area");
-let history = [];
+let gameOver = false;
+let chances = 7; //남은 기회
+let userValueList = []; //유저가 입력한 숫자들 리스트
 
+chanceArea.innerHTML = `남은 기회:${chances}`;
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
-//포커스 주면 입력한 숫자 사라짐
-userInput.addEventListener("focus", function() { 
-    userInput.value = "";
-});
+userInput.addEventListener("focus", focusInput);
 
-function pickRandomNum() {
-    computerNum = Math.floor(Math.random() * 100) + 1;
-    console.log("정답", computerNum);
+function pickRandomNumber() {
+    //랜덤 숫자 뽑기
+    computerNumber = Math.floor(Math.random() * 100) + 1;
+    console.log("정답", computerNumber);
 }
 
 function play() {
-    let userValue = userInput.value; 
-
+    //숫자 추축하기
+    const userValue = userInput.value; 
     if(userValue < 1 || userValue > 100) {
-        resultArea.textContent = "1과 100사이 숫자를 입력해 주세요.";
+        resultText.textContent = "1과 100사이 숫자를 입력";
+
         return; //함수 종료
     } 
     
-    if(history.includes(userValue)) { //이미 입력한 숫자인 경우 에러 메시지 출력 후 함수 종료
-        resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요.";
+    if(userValueList.includes(userValue)) { //이미 입력한 숫자인 경우 에러 메시지 출력 후 함수 종료
+        resultText.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요.";
+        
         return;
     }
 
     chances -- ;
-    chanceArea.textContent = `남은기회:${chances}번`; //텍스트가 아니라 변수 값이 들어옴
-    console.log("chance", chances);
-
-    if (userValue < computerNum) {
-        resultArea.textContent = "Up!";
+    chanceArea.textContent = `남은기회:${chances}`; //텍스트가 아니라 변수 값이 들어옴
+    userValueList.push(userValue);
+    if (userValue < computerNumber) {
+        resultAreaImg.src = 
+            "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnB6ODhnMTkwanFzYXpkM3BqeXRvOXljODhjaDUxZHBsZTI5cjdrNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT1R9Ie9mRoRs0w3C0/giphy.gif";
+        resultText.textContent = "Up!";
        
-    } else if (userValue > computerNum) {
-        resultArea.textContent = "Down!";
+    } else if (userValue > computerNumber) {
+        resultAreaImg.src = 
+            "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdzZ0amV1dGR0bmRqZXZkdDY0ZmQzNTJ1dXp6MHRxYmZ1a3Foa3N0aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT1R9QHP4ADiUoc4Ao/giphy.gif";
+        resultText.textContent = "Down!";
         
     } else {
-        resultArea.textContent = "맞췄습니다~";
+        resultAreaImg.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExenMwNGR3d2Y2Y2EweXZncmZucnA2cmdyaDVtdXE0aGZzdWptaDV3ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YFis3URdQJ6qA/giphy.gif"
+        resultText.textContent = "와우~정답!";
         gameOver = true; 
     }
-
-    history.push(userValue);
-    console.log(history);
 
     if(chances < 1) {
         gameOver = true;
@@ -60,12 +63,21 @@ function play() {
     }
 }
 
-function reset() {
+function focusInput() {
     userInput.value = "";
-
-    pickRandomNum();
-
-    resultArea.textContent = "결과값이 여기 나옵니다!";
 }
 
-pickRandomNum(); //함수 호출
+function reset() {
+    //리셋
+    pickRandomNumber();
+    userInput.value = "";
+    resultAreaImg.src = "images/up&down.gif";
+    resultText.textContent = "숫자를 맞추세요.";
+    gameOver = false;
+    playButton.disabled = false;
+    chances = 7;
+    chanceArea.innerHTML = `남은 기회:${chances}`;
+    userValueList = [];
+  }
+  
+  pickRandomNumber();
